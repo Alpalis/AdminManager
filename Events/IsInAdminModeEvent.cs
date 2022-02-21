9fb9b04d212e4e0c3085b61bd772ded824363d9a
Pandetthe <pandetthe@gmail.com>
2022-02-21 11:05:21 +0100

    AdminMode checking for UtilityServices

---
+++ Events/IsInAdminModeEvent.cs
@@ -0,0 +1,33 @@
+﻿using Alpalis.AdminManager.API;
+using Alpalis.UtilityServices.Events;
+using OpenMod.API.Eventing;
+using OpenMod.Core.Eventing;
+using Steamworks;
+using System.Threading.Tasks;
+
+namespace Alpalis.AdminManager.Events
+{
+    public class IsInAdminModeEvent : IEventListener<AdminModeEvent>
+    {
+        #region Member Variables
+        private readonly IAdminSystem m_AdminSystem;
+        #endregion Member Variables
+
+        #region Class Constructor
+        public IsInAdminModeEvent(
+            IAdminSystem adminSystem)
+        {
+            m_AdminSystem = adminSystem;
+        }
+        #endregion Class Constructor
+
+        [EventListener(Priority = EventListenerPriority.Normal)]
+        public async Task HandleEventAsync(object? sender, AdminModeEvent @event)
+        {
+            if (@event.Actor != null)
+                @event.IsInAdminMode = m_AdminSystem.IsInAdminMode(@event.Actor!);
+            if (@event.SteamID != null)
+                @event.IsInAdminMode = m_AdminSystem.IsInAdminMode((CSteamID)@event.SteamID);
+        }
+    }
+}
