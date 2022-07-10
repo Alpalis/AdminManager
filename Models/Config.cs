73827c53959157f32f1e692bd526ac3a35241a91
Jakub Jurczyk <pandetthe@gmail.com>
2022-07-10 13:32:09 +0200

    Update default config values

--- Models/Config.cs
+++ Models/Config.cs
@@ -6,10 +6,20 @@ namespace Alpalis.AdminManager.Models
     [Serializable]
     public class Config : MainConfig
     {
+        #region Class Constructor
         public Config()
         {
             MessagePrefix = true;
+            AdminUIID = 29000;
+            AdminUIKey = 0;
+            GodUIID = 29001;
+            GodUIKey = 1;
+            VanishUIID = 29002;
+            VanishUIKey = 2;
+            FlyUIID = 29003;
+            FlyUIKey = 3;
         }
+        #endregion Class Constructor
 
         public bool MessagePrefix { get; set; }
 
